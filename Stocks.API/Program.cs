b8a9c9db6102e228b90fe71a7ba6eacf58fb7217
Leopoldo Juárez <inheritech@gmail.com>
2021-02-08 19:22:59 -0600

    Added Serilog and startup exception handling

--- Stocks.API/Program.cs
+++ Stocks.API/Program.cs
@@ -1,16 +1,65 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Stocks.API {
     public class Program {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+
+        public static string EnvironmentName { get; } = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT)") ?? Environments.Production;
+
+        public static IConfiguration BaseConfiguration { get; } = GetBaseConfiguration();
+
+        public static async Task<int> Main(string[] args) {
+
+            Log.Logger = CreateDefaultSerilogLogger(BaseConfiguration);
+
+            try {
+                Log.Information("Configuring Stocks API host...");
+                var host = CreateHostBuilder(args).Build();
+
+                Log.Information("Starting Stocks API host...");
+                await host.RunAsync();
+
+                return 0;
+            } catch (Exception ex) {
+                Log.Fatal(ex, "Service terminated unexpectedly.");
+                return 1;
+            } finally {
+                Log.CloseAndFlush();
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.CaptureStartupErrors(false);
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static IConfiguration GetBaseConfiguration()
+            => new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        private static Serilog.ILogger CreateDefaultSerilogLogger(IConfiguration configuration)
+            => new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationContext", "Stocks.API")
+                .WriteTo.Debug()
+                .WriteTo.Console(
+                    outputTemplate: "[{Timestamp:HH:mm:ss}] ({Level:u3}) {Message:lj} {NewLine}{Exception}"
+                )
+                .CreateLogger();
+
     }
 }
