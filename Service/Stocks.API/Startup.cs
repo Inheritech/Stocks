dcf2919f316f60a14208c45c9480e1035b904849
Leopoldo Juárez <inheritech@gmail.com>
2021-02-15 11:40:22 -0600

    Added DB context to API project

--- Service/Stocks.API/Startup.cs
+++ Service/Stocks.API/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Stocks.Infrastructure;
+using System;
 
 namespace Stocks.API {
     public class Startup {
@@ -14,6 +17,16 @@ namespace Stocks.API {
 
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+
+            string connectionString = Configuration.GetConnectionString("StocksDb");
+
+            services.AddDbContext<StocksContext>(options => {
+                options.UseSqlServer(connectionString, sqlOpts => {
+                    sqlOpts.MigrationsHistoryTable("_MigrationsHistory", StocksContext.DefaultSchema);
+                    sqlOpts.MigrationsAssembly(typeof(StocksContext).Assembly.GetName().Name);
+                    sqlOpts.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                });
+            }, ServiceLifetime.Scoped);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
